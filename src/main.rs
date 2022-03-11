67b0bc4ca17e73d36148f09d61ac8b07f8531e45
Simon Gasse <sgasse@users.noreply.github.com>
2022-03-11 19:30:30 +0100

    Measure rough execution time

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use clap::Parser;
 use env_logger::TimestampPrecision;
 use git_copyright::{check_repo_copyright, Config};
+use std::time::Instant;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
@@ -15,7 +16,7 @@ struct Args {
     #[clap(short, long)]
     name: String,
 
-    /// TOML file with config to use
+    /// YAML file with config to use
     #[clap(short, long, default_value = "")]
     config: String,
 }
@@ -39,5 +40,8 @@ async fn main() {
         }
     };
 
+    let start = Instant::now();
     check_repo_copyright(&args.repo, &args.name, &config).await;
+    let duration_s = start.elapsed().as_millis() as f32 / 1000.0;
+    println!("Finished in {:0.3}s", duration_s);
 }
